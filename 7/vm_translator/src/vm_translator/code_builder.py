28b78361805d6b1fdefc38b09585866bd960617c
Max V. Kuznetsov <slonoten@gmail.com>
2026-09-14 10:20:41 +0300

    Project 7 sync

--- 7/vm_translator/src/vm_translator/code_builder.py
+++ 7/vm_translator/src/vm_translator/code_builder.py
@@ -1,21 +1,25 @@
+from textwrap import dedent
+
 from lark import Transformer
 
 
 def _push_d() -> str:
-    return """@SP
-A=M 
-M=D
-@SP
-M=M+1
-"""
+    return dedent("""
+        @SP
+        A=M 
+        M=D
+        @SP
+        M=M+1
+        """).strip()
 
 
 def _pop_d() -> str:
-    return """@SP
-M=M-1
-A=M
-D=M
-"""
+    return dedent("""
+        @SP
+        M=M-1
+        A=M
+        D=M
+        """).strip()
 
 
 def _copy_d_to_addr(base: str, offset: int) -> str:
@@ -47,14 +51,58 @@ def segment_to_d(segment: str) -> str:
     if segment == "local":
         return """
     """
+SEGMENT_TO_REG = {
+    "local" : "LCL",
+    "argument" : "ARG",
+    "this" : "THIS",
+    "that" : "THAT"
+}
+
+POINTER_INDEX_TO_REG = {
+    "0" : "THIS",
+    "1" : "THAT"
+}
+
+
+def _segment_to_d(segment: str, index: str) -> str:
+    
+    if reg := SEGMENT_TO_REG.get(segment):
+        return dedent(f"""
+            @{index}
+            D=A
+            @{reg}
+            A=D+M
+            D=M
+            """).strip()
+    elif segment == "constant":
+        return dedent(f"""
+            @{index}
+            D=A
+            """).strip()
+    elif segment == "pointer":
+        reg = POINTER_INDEX_TO_REG[index]
+        return dedent(f"""
+        @{reg}
+        D=M
+        """).sprip()
+    elif segment == "temp":
+        TMP = 5
+        address = TMP + int(index)
+        return dedent(f"""
+        @{address}
+        D=M
+        """).strip() 
+        
     raise NotImplemented("No code for {segment}")
 
 
 class CodeBuilder                                                                                       (Transformer):
     def push(self, args):
         segment, index = args
-        print(segment, index)
-        return "push"
+        return f"""// push {segment} {index}
+{_segment_to_d(segment, index)}
+{_push_d()}
+"""
 
     def program(self, commands):
         return "\n".join(map(str, commands))
